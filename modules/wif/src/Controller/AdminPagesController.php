a612f0c0de290642828f51a2ed3927c1bb5c13d6
Tamas Presing <tamas.presing@gmail.com>
2023-09-12 09:57:32 +0200

    BC-83: Wif install fix.

--- modules/wif/src/Controller/AdminPagesController.php
+++ modules/wif/src/Controller/AdminPagesController.php
@@ -12,7 +12,7 @@ use Drupal\Core\Controller\ControllerBase;
  * Provides route responses for the Webshop Image Formatter module.
  */
 class AdminPagesController extends ControllerBase {
-  
+
   /**
    * Returns the presets page.
    *
@@ -20,12 +20,12 @@ class AdminPagesController extends ControllerBase {
    *   A simple renderable array.
    */
   public function presets() {
-    $element = array(
+    $element = [
       '#markup' => 'Presets page goes here',
-    );
+    ];
     return $element;
   }
-  
+
   /**
    * Returns the library download page.
    *
@@ -33,59 +33,71 @@ class AdminPagesController extends ControllerBase {
    *   A simple renderable array.
    */
   public function downloadLibraries() {
-    
-    module_load_include('module', 'wif');
-    
+    if (\Drupal::hasService('extension.list.module')) {
+      /** @var \Drupal\Core\Extension\ModuleExtensionList $module_list */
+      $module_list = \Drupal::service('extension.list.module');
+      $file = DRUPAL_ROOT . '/' . $module_list->getPath('wif') . "/wif.module";
+      if (is_file($file)) {
+        require_once $file;
+      }
+    }
+    else {
+      return;
+    }
+
     // Load wif.settings configuration
     $wif_settings = \Drupal::config('wif.settings');
     // Get needed libraries array with information about the libraries
     $libraries = $wif_settings->get('needed_libraries');
-    
+
     // Get real path to the js files
-    foreach($libraries as $library_name => $library_settings) {
+    foreach ($libraries as $library_name => $library_settings) {
       $libraries[$library_name]['path'] = _wif_get_path($library_name, $library_settings['path_to_js']);
     }
-    
+
     // Create table rows for the libraries
-    $rows = array();
+    $rows = [];
     $installation_counter = 0;
-    foreach($libraries as $library_name => $library) {
-      $rows[] = array(
-        'data' => array(
+    foreach ($libraries as $library_name => $library) {
+      $rows[] = [
+        'data' => [
           $library['name'],
-          array(
-            'data' => ($library['path'])? t('Installed successfully!') : t('Library is not found.'),
-            'class' => ($library['path'])? 'messages messages--status' : 'messages messages--error',
-          ),
+          [
+            'data' => ($library['path']) ? t('Installed successfully!') : t('Library is not found.'),
+            'class' => ($library['path']) ? 'messages messages--status' : 'messages messages--error',
+          ],
           'Install button goes here',
-        ),
-      );
-      
+        ],
+      ];
+
       // If this library is installed, increase the installation_counter
-      if ($library['path']) $installation_counter++;
+      if ($library['path']) {
+        $installation_counter++;
+      }
     }
-    
+
     if ($installation_counter == 3) {
-      $rows[] = array(
-        'data' => array(
-          array(
+      $rows[] = [
+        'data' => [
+          [
             'data' => t('All needed libraries is installed successfully!'),
             'class' => 'messages messages--status',
             'colspan' => 3,
-          ),
-        ),
-      );
-    } else {
-      
+          ],
+        ],
+      ];
     }
-    
-    $element = array(
+    else {
+    }
+
+    $element = [
       '#theme' => 'table',
-      '#header' => array(t('Library'), t('Status'), t('Action')),
+      '#header' => [t('Library'), t('Status'), t('Action')],
       '#rows' => $rows,
-    );
+    ];
     return $element;
   }
 
 }
+
 ?>
