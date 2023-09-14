bc2d0f33a79e447b715fd1f6c62e017e477e5065
Tamas Presing <tamas.presing@gmail.com>
2023-09-14 13:49:20 +0200

    BC-9: Show payment methods when only one available.

--- modules/commerce/src/Plugin/Commerce/CheckoutPane/PaymentMethod.php
+++ modules/commerce/src/Plugin/Commerce/CheckoutPane/PaymentMethod.php
@@ -37,6 +37,11 @@ class PaymentMethod extends BasePaymentInformation {
       }
     }
 
+    // Show payment method form when only one option is available.
+    if (count($pane_form['#payment_options']) < 2) {
+      $pane_form['payment_method']['#access'] = TRUE;
+    }
+
     return $pane_form;
   }
 
