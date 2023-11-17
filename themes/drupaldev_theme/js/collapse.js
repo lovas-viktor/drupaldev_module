d7e1ccccf7da8116abe5ba122bb1ffcba6b2e5ec
Anett Antal <antalanett90@gmail.com>
2023-11-17 09:13:22 +0100

    slinky menu clickable expanded menu item solution

--- themes/drupaldev_theme/js/collapse.js
+++ themes/drupaldev_theme/js/collapse.js
@@ -5,48 +5,60 @@
       let windowsize = $(window).width();
       let last_target;
 
-      $(window).resize(function() {
+      $(window).resize(function () {
         windowsize = $(window).width();
       });
 
-      function menuInit(){
+      function menuInit() {
 
-        $(".collapse-toggler").once('clickToToggler').on('click',function(event){
+        $(".collapse-toggler").once('clickToToggler').on('click', function (event) {
 
           const target = $(this).attr('data-target');
           last_target = '#' + $('.collapse-content.opened').attr('id');
 
-          if(target && target!== last_target){
+          if (target && target !== last_target) {
             closeCollapse(last_target);
             openCollapse(target);
           }
 
-          if (target && target == last_target){
+          if (target && target == last_target) {
             closeCollapse(last_target);
           }
         });
 
-        $('.collapse-overlay').once('collapseOverlay').on('click',function(){
+        $('.collapse-overlay').once('collapseOverlay').on('click', function () {
           closeCollapse();
         });
       }
 
-      function openCollapse(target){
+      function openCollapse(target) {
         $('body').addClass('collapse-opened');
-        $( target ).toggleClass('opened');
-        if($( target ).find('.slinky-menu--mobile')){
-          console.log('slinky');
-          if(windowsize < 768){
-            $(".slinky-menu--mobile").once('slinkyOnce').each(function(){
+        $(target).toggleClass('opened');
+        if ($(target).find('.slinky-menu--mobile')) {
+          if (windowsize < 768) {
+            $(".slinky-menu--mobile").once('slinkyOnce').each(function () {
+
               $(this).once('slinkyInit').slinky({
                 title: true
               });
+
+              $('.slinky-menu li.menu-item--expanded').each(function () {
+                $(this).children().children().children().children('.menu-title').click(function(){
+                  $('.slinky-theme-default').addClass('slinky-linkable')
+                  var link= $(this).parents('a').attr('href');
+                  $('body').removeClass('collapse-opened');
+                  $(target).toggleClass('opened');
+                  window.location.href = link;
+
+                })
+              });
+
             });
           }
         }
       }
 
-      function closeCollapse(target){
+      function closeCollapse(target) {
         $('body').removeClass('collapse-opened');
         $('.collapse-content').removeClass('opened');
       }
