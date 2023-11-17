69c7db564bbe284f682fac254814be3101ad7d59
Tamas Presing <tamas.presing@gmail.com>
2023-11-17 10:07:04 +0100

    BC-141: Breadcrumb fixes.

--- modules/commerce/src/CustomBreadcrumbs.php
+++ modules/commerce/src/CustomBreadcrumbs.php
@@ -7,6 +7,7 @@ use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
 use Drupal\Core\Link;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\drupaldev_search\Entity\DrupaldevSearchAlias;
 use Drupal\node\NodeInterface;
 
 /**
@@ -15,42 +16,94 @@ use Drupal\node\NodeInterface;
  * @package Drupal\custom_breadcrumb
  */
 class CustomBreadcrumbs implements BreadcrumbBuilderInterface {
+
   use StringTranslationTrait;
 
   /**
    * {@inheritDoc}
    */
   public function applies(RouteMatchInterface $route_match) {
-    return $route_match->getRouteName() == 'entity.commerce_product.canonical';
+    return $route_match->getRouteName() == 'entity.commerce_product.canonical' || str_contains($route_match->getRouteName(), 'drupaldev.search_route_');
   }
 
   /**
    * {@inheritDoc}
    */
   public function build(RouteMatchInterface $route_match) {
-    $commerce_product = $route_match->getParameter('commerce_product');
+    // Route paramters.
+    $facet_param = $route_match->getParameter('f0');
+
     $breadcrumb = new Breadcrumb();
+
+    // Add "Home" breadcrumb link.
+    $breadcrumb->addLink(Link::createFromRoute($this->t('Home'), '<front>'));
+
+    // Add "Products" breadcrumb link.
+    $products_link = Link::createFromRoute(t('Products'), 'view.products.page_1');
+    $breadcrumb->addLink($products_link);
 
     // By setting a "cache context" to the "url", each requested URL gets it's
     // own cache. This way a single breadcrumb isn't cached for all pages on the
     // site.
     $breadcrumb->addCacheContexts(["url"]);
+    $commerce_product = $route_match->getParameter('commerce_product');
 
-    // By adding "cache tags" for this specific node, the cache is invalidated
-    // when the node is edited.
-    $breadcrumb->addCacheTags(["commerce_product:{$commerce_product->id()}"]);
+    if ($commerce_product) {
+      // By adding "cache tags" for this specific node, the cache is invalidated
+      // when the node is edited.
+      $breadcrumb->addCacheTags(["commerce_product:{$commerce_product->id()}"]);
 
-    // Add "Home" breadcrumb link.
-    $breadcrumb->addLink(Link::createFromRoute($this->t('Home'), '<front>'));
+      $catalog_terms = $commerce_product->get('field_catalog')
+        ->referencedEntities();
 
-    $catalog_terms = $commerce_product->get('field_catalog')->referencedEntities();
-    if (!empty($catalog_terms)) {
-      foreach ($catalog_terms as $key => $term) {
-        if ($key !== array_key_last($catalog_terms)) {
-          $breadcrumb->addLink($term->toLink());
+      $parent_terms = \Drupal::entityTypeManager()
+        ->getStorage('taxonomy_term')
+        ->loadAllParents($catalog_terms[0]->id());
+
+      $parent_terms = array_reverse($parent_terms, TRUE);
+      if (!empty($parent_terms)) {
+        foreach ($parent_terms as $key => $term) {
+          // If not last element or only one element available add it to the breadcrumb.
+          if ($key != array_key_last($parent_terms) || count($parent_terms) == 1) {
+            $breadcrumb->addLink($term->toLink());
+          }
         }
       }
     }
+
+    if (!empty($facet_param)) {
+      $query = \Drupal::entityQuery('drupaldev_search_alias');
+      $conditionGroup = $query->andConditionGroup();
+      $conditionGroup->condition('alias', $facet_param);
+      $query->condition($conditionGroup);
+      $query->condition('langcode', \Drupal::languageManager()
+        ->getCurrentLanguage()
+        ->getId());
+      $results = $query->execute();
+
+      if (empty($results)) {
+        return $breadcrumb;
+      }
+
+      $search_alias = DrupaldevSearchAlias::load(reset($results));
+      $path_array = explode('&', $search_alias->getPath());
+      $last_path_filter = explode(':', end($path_array));
+      $last_term_id = $last_path_filter[1];
+
+      $parent_terms = \Drupal::entityTypeManager()
+        ->getStorage('taxonomy_term')
+        ->loadAllParents($last_term_id);
+      $parent_terms = array_reverse($parent_terms, TRUE);
+
+      if (!empty($parent_terms)) {
+        foreach ($parent_terms as $key => $term) {
+          if ($key != array_key_last($parent_terms)) {
+            $breadcrumb->addLink($term->toLink());
+          }
+        }
+      }
+    }
+
     return $breadcrumb;
   }
 
