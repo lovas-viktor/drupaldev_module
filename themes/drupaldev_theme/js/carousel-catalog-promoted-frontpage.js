db02f17fde94048e41d45cb93c6d1befb4ecc8ac
Anett Antal <antalanett90@gmail.com>
2023-12-14 09:51:24 +0100

    catalog promoted to frontpage

--- themes/drupaldev_theme/js/carousel-catalog-promoted-frontpage.js
+++ themes/drupaldev_theme/js/carousel-catalog-promoted-frontpage.js
@@ -1,48 +1,28 @@
 (function ($, Drupal) {
-  Drupal.behaviors.initFrontpageCatalogCarousel = {
+  Drupal.behaviors.initFrontpagePromotedCatalogCarousel = {
     attach: function (context, settings) {
 
-      var $owl = $('.owl-carousel--frontpage-catalog');
-
-      $('.frontpage-owl-wrapper').each(function(){
-        var owlItems = $(this).find('.owl-carousel--frontpage-catalog').length;
-
-        for (var i = 0; i < owlItems; i++) {
-          var actualItem = i + 1;
-          $(this).find('.owl-carousel--frontpage-catalog').eq(i).addClass('owl-carousel-' + actualItem);
-        }
-
-        $owl.owlCarousel({
-          items: 1,
-          loop: true,
-          mouseDrag: false,
-          touchDrag: false,
-          pullDrag: false,
-          dots: false,
-          autoplay: true,
-          animateOut: 'fadeOut',
-          animateIn: 'fadeIn',
-          autoplayTimeout: owlItems * 2000,
+      $('.owl-catalog-promoted-frontpage').each(function(){
+        $(this).owlCarousel({
+          //loop: true,
+          margin:30,
+          dotsSpeed: 1000,
+          responsive:{
+            0:{
+              items: 2,
+              mouseDrag: false,
+              touchDrag: true
+            },
+            767:{
+              items:4,
+              mouseDrag: false,
+              touchDrag: true,
+            },
+            991:{
+              items:6
+            }
+          }
         });
-
-      });
-
-      $('.owl-carousel--frontpage-catalog').trigger('stop.owl.autoplay');
-
-      var items = {
-        itemindex: [],
-        itemname: [],
-        itemdelay: [],
-      };
-      $owl.each(function (index) {
-        var delay = (index + 1) *2000;
-        items.itemindex.push(index);
-        items.itemname.push('.owl-carousel-' + (index + 1));
-        items.itemdelay.push(delay);
-
-        setTimeout(function() {
-          $(items.itemname[index]).trigger('play.owl.autoplay');
-        }, items.itemdelay[index]);
       });
 
     }
