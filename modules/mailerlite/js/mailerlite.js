a1dee3404d68f30b4206ec3ecda2da3550de2ee8
Tamas Presing <tamas.presing@gmail.com>
2024-02-09 10:46:08 +0100

    Birth dateOC

--- modules/mailerlite/js/mailerlite.js
+++ modules/mailerlite/js/mailerlite.js
@@ -1,15 +1,18 @@
 (function ($, Drupal) {
   Drupal.behaviors.mailerlite = {
     attach: function (context, settings) {
-
-      $("#datepicker").datepicker({
-        dateFormat: 'yy-mm-dd',
-        changeMonth: true,
-        changeYear: false,
-        minDate: new Date(2024, 0, 0),
-        maxDate: new Date(2024, 11, 31),
-        yearRange: "-0:+0"
-      });
+      if ($.fn.datepicker) {
+        $('#datepicker').datepicker({
+          dateFormat: 'M d',
+          changeMonth: true,
+          changeYear: false,
+          minDate: new Date(2024, 0, 0),
+          maxDate: new Date(2024, 11, 31),
+          yearRange: "-0:+0",
+        });
+      } else {
+        console.error('No datepicker library found');
+      }
     }
   };
 })(jQuery, Drupal);
