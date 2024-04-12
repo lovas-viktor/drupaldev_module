db73e5aab97e43ca778b2eacac796feb445c36f0
Tamas Presing <tamas.presing@gmail.com>
2024-04-12 12:40:42 +0200

    hotfix/furgefutar

--- modules/furgefutar/src/Form/ShippingSelectForm.php
+++ modules/furgefutar/src/Form/ShippingSelectForm.php
@@ -89,7 +89,7 @@ class ShippingSelectForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     if ($this->order instanceof Order) {
-      \Drupal::messenger()->addMessage('Enviroment: ' . $this->env);
+      //\Drupal::messenger()->addMessage('Enviroment: ' . $this->env);
       $existing_quote = $this->furgefutarService->getQuotesForOrder($this->order);
 
       // If no quote yet, display the form.
