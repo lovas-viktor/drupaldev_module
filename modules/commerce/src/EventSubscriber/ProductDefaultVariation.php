440e8262d18f0bb1df760658655b6114567d1f61
Tamas Presing <tamas.presing@gmail.com>
2024-05-03 09:40:07 +0200

    BC-356: Cheapest product variation.

---
+++ modules/commerce/src/EventSubscriber/ProductDefaultVariation.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\drupaldev_commerce\EventSubscriber;
+
+use Drupal\commerce_product\Entity\ProductVariation;
+use Drupal\commerce_product\Event\ProductDefaultVariationEvent;
+use Drupal\commerce_product\Event\ProductEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * @todo Add description for this subscriber.
+ */
+final class ProductDefaultVariation implements EventSubscriberInterface {
+
+  public function getDefaultVariation(ProductDefaultVariationEvent $event) {
+    if ($event->getProduct()->bundle() == 'default') {
+      $variations = $event->getProduct()->getVariations();
+      $variation_price_map = [];
+
+      foreach ($variations as $variation) {
+        $variation_price_map[$variation->id()] = $variation->getPrice()->getNumber();
+      }
+
+      /** @var ProductVariation $cheapest_variation */
+      $cheapest_variation = ProductVariation::load(min(array_keys($variation_price_map)));
+      $event->setDefaultVariation($cheapest_variation);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    return [
+      ProductEvents::PRODUCT_DEFAULT_VARIATION => [
+        'getDefaultVariation',
+        -100,
+      ],
+    ];
+  }
+
+}
