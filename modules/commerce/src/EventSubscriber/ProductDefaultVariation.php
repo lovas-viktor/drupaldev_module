7b64bd94f58b15ee9b3e5a84576dac703ac5a46c
Tamas Presing <tamas.presing@gmail.com>
2024-05-16 16:36:51 +0200

    BC-381: Fix default product variation select.

--- modules/commerce/src/EventSubscriber/ProductDefaultVariation.php
+++ modules/commerce/src/EventSubscriber/ProductDefaultVariation.php
@@ -20,15 +20,19 @@ final class ProductDefaultVariation implements EventSubscriberInterface {
   public function getDefaultVariation(ProductDefaultVariationEvent $event) {
     if ($event->getProduct()->bundle() == 'default') {
       $variations = $event->getProduct()->getVariations();
-      $variation_price_map = [];
+      if (!empty($variations)) {
+        $variation_price_map = [];
 
-      foreach ($variations as $variation) {
-        $variation_price_map[$variation->id()] = $variation->getPrice()->getNumber();
+        foreach ($variations as $variation) {
+          $variation_price_map[$variation->id()] = $variation->getPrice()
+            ->getNumber();
+        }
+
+        /** @var ProductVariation $cheapest_variation */
+        $cheapest_variation = ProductVariation::load(min(array_keys($variation_price_map)));
+        $event->setDefaultVariation($cheapest_variation);
       }
 
-      /** @var ProductVariation $cheapest_variation */
-      $cheapest_variation = ProductVariation::load(min(array_keys($variation_price_map)));
-      $event->setDefaultVariation($cheapest_variation);
     }
   }
 
