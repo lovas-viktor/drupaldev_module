40e9815f4df6e3cdcc1e52a2c8826f8efd325eff
Viktor Lovas <viktor.lovas@gmail.com>
2024-05-24 06:50:14 +0200

    Add brand to term redirect

--- modules/search/src/EventSubscriber/TermRedirectSubscriber.php
+++ modules/search/src/EventSubscriber/TermRedirectSubscriber.php
@@ -32,7 +32,7 @@ class TermRedirectSubscriber implements EventSubscriberInterface {
       $facets = Facet::loadMultiple();
 
       foreach ($facets as $id => $facet) {
-        if($id != 'catalog' && $id != 'tags'){
+        if($id != 'catalog' && $id != 'tags' && $id != 'brand'){
           continue;
         }
         $field_identifier = $facet->getFieldIdentifier();
