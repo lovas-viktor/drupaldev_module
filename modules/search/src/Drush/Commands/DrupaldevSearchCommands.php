61eaa0231f172a5eff99b64862070b7c9e3f07e7
Viktor Lovas <viktor.lovas@gmail.com>
2024-10-22 12:00:46 +0200

    Create cron search alias cleaner

--- modules/search/src/Drush/Commands/DrupaldevSearchCommands.php
+++ modules/search/src/Drush/Commands/DrupaldevSearchCommands.php
@@ -34,34 +34,7 @@ final class DrupaldevSearchCommands extends DrushCommands {
   #[CLI\Argument(name: 'created_before', description: 'Clean aliases created before the given date.')]
   #[CLI\Usage(name: 'drupaldev_search:command-name foo', description: 'Usage description')]
   public function commandName($created_before = "") {
-    $database = \Drupal::database();
-    if (!empty($created_before)) {
-      $timestamp = strtotime($created_before . ' 00:00:00');
-      $q = "SELECT id, `param_count` FROM ( SELECT id, LENGTH(`query_path`) - LENGTH(REPLACE(`query_path`, '&', '')) AS `param_count`, created_at FROM drupaldev_search_alias ) AS subquery WHERE created_at <= $timestamp AND param_count <= 2;";
-      $query = $database->query($q);
-      $result = $query->fetchAll();
-    }
-    else {
-      $query = $database->query("SELECT id, `param_count` FROM ( SELECT id, LENGTH(`query_path`) - LENGTH(REPLACE(`query_path`, '&', '')) AS `param_count`, created_at FROM drupaldev_search_alias ) AS subquery WHERE created_at <= now() - INTERVAL 1 DAY AND param_count <= 2;");
-      $result = $query->fetchAll();
-    }
-
-    $this->logger()->success(dt(count($result) . ' rows found.'));
-
-    // Prepare array.
-    $ids_to_delete = [];
-    $num_deleted = 0;
-    foreach ($result as $res) {
-      $ids_to_delete[] = $res->id;
-    }
-
-    if (!empty($ids_to_delete)) {
-      $num_deleted = $database->delete('drupaldev_search_alias')
-        ->condition('id', $ids_to_delete, 'IN')
-        ->execute();
-    }
-
-    $this->logger()->success(dt($num_deleted . ' rows deleted.'));
+      drupaldev_search_clean_aliases ($created_before);
   }
 
 }
